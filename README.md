e34e77f7c1b8da243d1ddb7fc619c709d8c9124f
Tanni-Dey <tonnidey277615@gmail.com>
2023-07-25 16:22:49 +0600

    books-get-api

---
+++ README.md
@@ -0,0 +1,3 @@
+[Smart Library Frontend Code Link](https://github.com/Tanni-Dey/smart-library-frontend)
+[Smart Library Backend Code Link](https://github.com/Tanni-Dey/smart-library-backend)
+[Smart Library Live Link](https://www.google.com)
---
+++ index.ts
@@ -0,0 +1,42 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+require("dotenv").config();
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1tyqf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+} as object);
+
+const run = async () => {
+  try {
+    const db = client.db("smart-library");
+    const bookCollection = db.collection("books");
+
+    app.get("/books", async (req: Request, res: Response) => {
+      const findBooks = bookCollection.find({});
+      const allBooks = await findBooks.toArray();
+      res.send({ sucess: true, data: allBooks });
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("smart-library");
+});
+
+app.listen(port, () => {
+  console.log(`smart-library app listening on port ${port}`);
+});
